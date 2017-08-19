c1723c991b4168dfdf12e30a0b250c7c058b6ced
Michael Heasell <michael.heasell@gmail.com>
2017-08-19 22:55:45 +0100

    Add some more color palette code

--- src/rwe/ColorPalette.cpp
+++ src/rwe/ColorPalette.cpp
@@ -2,7 +2,12 @@
 
 namespace rwe
 {
-    Color::Color(unsigned char r, unsigned char g, unsigned char b) : r(r), g(g), b(b) {}
+    const Color Color::Black = Color(0, 0, 0, 255);
+    const Color Color::Transparent = Color(0, 0, 0, 0);
+
+    Color::Color(unsigned char r, unsigned char g, unsigned char b) noexcept : Color(r, g, b, 255) {}
+
+    Color::Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a) noexcept : r(r), g(g), b(b), a(a) {}
 
     void readColorPalette(const char* input, Color* output)
     {
@@ -12,5 +17,22 @@ namespace rwe
             output[i].g = static_cast<unsigned char>(input[(3 * i) + 1]);
             output[i].b = static_cast<unsigned char>(input[(3 * i) + 2]);
         }
+    }
+
+    boost::optional <ColorPalette> readPalette(std::vector<char>& vector)
+    {
+        assert(vector.size() >= (3 * 256));
+
+        std::vector<Color> colors(256);
+
+        for (unsigned int i = 0; i < 256; ++i)
+        {
+            colors[i].r = static_cast<unsigned char>(vector[(3 * i)]);
+            colors[i].g = static_cast<unsigned char>(vector[(3 * i) + 1]);
+            colors[i].b = static_cast<unsigned char>(vector[(3 * i) + 2]);
+            colors[i].a = 255;
+        }
+
+        return colors;
     }
 }
--- src/rwe/ColorPalette.h
+++ src/rwe/ColorPalette.h
@@ -2,20 +2,33 @@
 #define RWE_COLORPALETTE_H
 
 #include <cstdint>
+#include <vector>
+#include <boost/optional.hpp>
 
 namespace rwe
 {
+#pragma pack(1)
     struct Color
     {
-        unsigned char r;
-        unsigned char g;
-        unsigned char b;
+        static const Color Black;
+        static const Color Transparent;
+
+        uint8_t r;
+        uint8_t g;
+        uint8_t b;
+        uint8_t a;
 
         Color() = default;
-        Color(unsigned char r, unsigned char g, unsigned char b);
+        Color(unsigned char r, unsigned char g, unsigned char b) noexcept;
+        Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a) noexcept;
     };
+#pragma pack()
+
+    using ColorPalette = std::vector<Color>;
 
     void readColorPalette(const char* input, Color* output);
+
+    boost::optional<ColorPalette> readPalette(std::vector<char>& vector);
 }
 
 #endif
