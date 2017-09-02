212c8335a221c91d058cd654ab522184b8f3680c
Michael Heasell <michael.heasell@gmail.com>
2017-09-02 08:55:13 +0100

    Move UiPanel code to a source file

--- src/rwe/ui/UiPanel.cpp
+++ src/rwe/ui/UiPanel.cpp
@@ -3,4 +3,137 @@
 namespace rwe
 {
 
+    UiPanel::UiPanel(int posX, int posY, unsigned int sizeX, unsigned int sizeY, SharedTextureHandle background)
+            : UiComponent(posX, posY, sizeX, sizeY),
+              background(std::move(background))
+    {
+    }
+
+    UiPanel::UiPanel(UiPanel&& panel) noexcept
+            : UiComponent(panel.posX, panel.posY, panel.sizeX, panel.sizeY),
+              background(std::move(panel.background)),
+              children(std::move(panel.children)),
+              focusedChild(std::move(panel.focusedChild))
+    {
+    }
+
+    UiPanel& UiPanel::operator=(UiPanel&& panel) noexcept
+    {
+        posX = panel.posX;
+        posY = panel.posY;
+        sizeX = panel.sizeX;
+        sizeY = panel.sizeY;
+        background = panel.background;
+        children = std::move(panel.children);
+        focusedChild = std::move(panel.focusedChild);
+
+        return *this;
+    }
+
+    void UiPanel::render(GraphicsContext& graphics) const
+    {
+        graphics.drawTexture(0, 0, sizeX, sizeY, background);
+        for (const auto& i : children)
+        {
+            i->render(graphics);
+        }
+    }
+
+    void UiPanel::mouseDown(MouseButtonEvent event)
+    {
+        auto size = children.size();
+        for (std::size_t i = 0; i < size; ++i)
+        {
+            auto& c = children[i];
+            if (c->contains(event.x, event.y))
+            {
+                setFocus(i);
+                c->mouseDown(event);
+                return;
+            }
+        }
+    }
+
+    void UiPanel::mouseUp(MouseButtonEvent event)
+    {
+        if (!focusedChild)
+        {
+            return;
+        }
+
+        children[*focusedChild]->mouseUp(event);
+    }
+
+    void UiPanel::keyDown(KeyEvent event)
+    {
+        if (!focusedChild)
+        {
+            return;
+        }
+
+        children[*focusedChild]->keyDown(event);
+    }
+
+    void UiPanel::keyUp(KeyEvent event)
+    {
+        if (!focusedChild)
+        {
+            return;
+        }
+
+        children[*focusedChild]->keyUp(event);
+    }
+
+    void UiPanel::mouseMove(MouseMoveEvent event)
+    {
+        UiComponent::mouseMove(event);
+        for (auto& e : children)
+        {
+            e->mouseMove(event);
+        }
+    }
+
+    void UiPanel::focus()
+    {
+        if (!focusedChild)
+        {
+            children[*focusedChild]->focus();
+        }
+    }
+
+    void UiPanel::unfocus()
+    {
+        if (!focusedChild)
+        {
+            children[*focusedChild]->unfocus();
+        }
+    }
+
+    void UiPanel::appendChild(std::unique_ptr<UiComponent>&& c)
+    {
+        children.push_back(std::move(c));
+    }
+
+    void UiPanel::setFocus(std::size_t controlIndex)
+    {
+        assert(controlIndex < children.size());
+
+        if (focusedChild)
+        {
+            children[*focusedChild]->unfocus();
+        }
+
+        focusedChild = controlIndex;
+        children[*focusedChild]->focus();
+    }
+
+    void UiPanel::clearFocus()
+    {
+        if (focusedChild)
+        {
+            children[*focusedChild]->unfocus();
+        }
+
+        focusedChild = boost::none;
+    }
 }
--- src/rwe/ui/UiPanel.h
+++ src/rwe/ui/UiPanel.h
@@ -17,145 +17,39 @@ namespace rwe
         boost::optional<std::size_t> focusedChild{boost::none};
 
     public:
-        UiPanel(int posX, int posY, unsigned int sizeX, unsigned int sizeY, SharedTextureHandle background)
-            : UiComponent(posX, posY, sizeX, sizeY),
-              background(std::move(background))
-        {
-        }
+        UiPanel(int posX, int posY, unsigned int sizeX, unsigned int sizeY, SharedTextureHandle background);
 
         UiPanel(const UiPanel&) = delete;
         UiPanel& operator=(const UiPanel&) = delete;
 
-        UiPanel(UiPanel&& panel) noexcept
-            : UiComponent(panel.posX, panel.posY, panel.sizeX, panel.sizeY),
-              background(std::move(panel.background)),
-              children(std::move(panel.children)),
-              focusedChild(std::move(panel.focusedChild))
-        {
-        }
+        UiPanel(UiPanel&& panel) noexcept;
 
-        UiPanel& operator=(UiPanel&& panel) noexcept
-        {
-            posX = panel.posX;
-            posY = panel.posY;
-            sizeX = panel.sizeX;
-            sizeY = panel.sizeY;
-            background = panel.background;
-            children = std::move(panel.children);
-            focusedChild = std::move(panel.focusedChild);
-
-            return *this;
-        }
+        UiPanel& operator=(UiPanel&& panel) noexcept;
 
         ~UiPanel() override = default;
 
-        void render(GraphicsContext& graphics) const override
-        {
-            graphics.drawTexture(0, 0, sizeX, sizeY, background);
-            for (const auto& i : children)
-            {
-                i->render(graphics);
-            }
-        }
+        void render(GraphicsContext& graphics) const override;
 
-        void mouseDown(MouseButtonEvent event) override
-        {
-            auto size = children.size();
-            for (std::size_t i = 0; i < size; ++i)
-            {
-                auto& c = children[i];
-                if (c->contains(event.x, event.y))
-                {
-                    setFocus(i);
-                    c->mouseDown(event);
-                    return;
-                }
-            }
-        }
+        void mouseDown(MouseButtonEvent event) override;
 
-        void mouseUp(MouseButtonEvent event) override
-        {
-            if (!focusedChild)
-            {
-                return;
-            }
+        void mouseUp(MouseButtonEvent event) override;
 
-            children[*focusedChild]->mouseUp(event);
-        }
+        void keyDown(KeyEvent event) override;
 
-        void keyDown(KeyEvent event) override
-        {
-            if (!focusedChild)
-            {
-                return;
-            }
+        void keyUp(KeyEvent event) override;
 
-            children[*focusedChild]->keyDown(event);
-        }
+        void mouseMove(MouseMoveEvent event) override;
 
-        void keyUp(KeyEvent event) override
-        {
-            if (!focusedChild)
-            {
-                return;
-            }
+        void focus() override;
 
-            children[*focusedChild]->keyUp(event);
-        }
+        void unfocus() override;
 
-        void mouseMove(MouseMoveEvent event) override
-        {
-            UiComponent::mouseMove(event);
-            for (auto& e : children)
-            {
-                e->mouseMove(event);
-            }
-        }
-
-        void focus() override
-        {
-            if (!focusedChild)
-            {
-                children[*focusedChild]->focus();
-            }
-        }
-
-        void unfocus() override
-        {
-            if (!focusedChild)
-            {
-                children[*focusedChild]->unfocus();
-            }
-        }
-
-        void appendChild(std::unique_ptr<UiComponent>&& c)
-        {
-            children.push_back(std::move(c));
-        }
+        void appendChild(std::unique_ptr<UiComponent>&& c);
 
     private:
-        void setFocus(std::size_t controlIndex)
-        {
-            assert(controlIndex < children.size());
+        void setFocus(std::size_t controlIndex);
 
-            if (focusedChild)
-            {
-                children[*focusedChild]->unfocus();
-            }
-
-            focusedChild = controlIndex;
-            children[*focusedChild]->focus();
-        }
-
-        void clearFocus()
-        {
-            if (focusedChild)
-            {
-                children[*focusedChild]->unfocus();
-            }
-
-            focusedChild = boost::none;
-        }
+        void clearFocus();
     };
 }
 
