a6f60aced639c477e6f08569fd4c280ffed62045
Michael Heasell <michael.heasell@gmail.com>
2017-09-04 21:53:56 +0100

    Maybe fix curl

--- appveyor_test.bat
+++ appveyor_test.bat
@@ -4,7 +4,7 @@ SET MSYSTEM=MINGW64
 rem Test the project
 C:\msys64\usr\bin\bash -lc "cd $APPVEYOR_BUILD_FOLDER && cd build && ./rwe_test -r junit -o test_output.xml" || goto :error
 
-C:\msys64\usr\bin\bash -lc "curl https://ci.appveyor.com/api/testresults/junit/$APPVEYOR_JOB_ID -F file=$APPVEYOR_BUILD_FOLDER/build/test_output.xml" || goto :error
+C:\msys64\usr\bin\bash -lc "curl https://ci.appveyor.com/api/testresults/junit/$APPVEYOR_JOB_ID -F file=@$APPVEYOR_BUILD_FOLDER/build/test_output.xml" || goto :error
 
 goto :EOF
 
