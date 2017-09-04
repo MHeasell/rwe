d0d7fd7a92c1dc23dd6c7d690de19f36964d6c80
Michael Heasell <michael.heasell@gmail.com>
2017-09-04 22:10:24 +0100

    Don't test during build

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -2,7 +2,7 @@ rem Required for bash to set itself up
 SET MSYSTEM=MINGW64
 
 rem Build the project
-C:\msys64\usr\bin\bash -lc "cd $APPVEYOR_BUILD_FOLDER && mkdir build && cd build && cmake -G 'Unix Makefiles' .. && make -j 2 && ./rwe_test" || goto :error
+C:\msys64\usr\bin\bash -lc "cd $APPVEYOR_BUILD_FOLDER && mkdir build && cd build && cmake -G 'Unix Makefiles' .. && make -j 2" || goto :error
 
 rem Create the build artifacts
 C:\msys64\usr\bin\bash -lc "cd $APPVEYOR_BUILD_FOLDER/build && make -j 2 package" || goto :error
