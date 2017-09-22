d9bc777a42b1a4e78b57630a3c5203616f89d310
Michael Heasell <michael.heasell@gmail.com>
2017-09-22 02:09:52 +0100

    Don't use blending when drawing map terrain

--- src/rwe/GraphicsContext.cpp
+++ src/rwe/GraphicsContext.cpp
@@ -330,9 +330,8 @@ namespace rwe
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-        // enable blending
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        // disable blending
+        glDisable(GL_BLEND);
 
         std::unordered_map<GLuint, std::vector<std::pair<unsigned int, unsigned int>>> batches;
 
