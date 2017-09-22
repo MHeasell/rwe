605246d47668ca1e10e36fc799f134857e78ff6e
Michael Heasell <michael.heasell@gmail.com>
2017-09-22 01:48:04 +0100

    Explicitly disable mipmaps when creating textures

--- src/rwe/GraphicsContext.cpp
+++ src/rwe/GraphicsContext.cpp
@@ -157,6 +157,8 @@ namespace rwe
         SharedTextureHandle handle(texture);
 
         glBindTexture(GL_TEXTURE_2D, texture);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
         glTexImage2D(
             GL_TEXTURE_2D,
             0,
