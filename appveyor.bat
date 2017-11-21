e174c039ea4980852bad957bd2563cc550a9dbdd
Michael Heasell <michael.heasell@gmail.com>
2017-11-21 22:01:24 +0000

    Fix AppVeyor always building the debug build

--- appveyor.bat
+++ appveyor.bat
@@ -11,7 +11,7 @@ rem Install project dependencies
 C:\msys64\usr\bin\bash -lc "pacman -Sq --needed --noconfirm mingw-w64-x86_64-boost mingw-w64-x86_64-SDL2 mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-SDL2_mixer mingw-w64-x86_64-SDL2_net mingw-w64-x86_64-glew mingw-w64-x86_64-smpeg2" || goto :error
 
 rem Build the project
-C:\msys64\usr\bin\bash -lc "cd $APPVEYOR_BUILD_FOLDER && mkdir build && cd build && cmake -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE=$CONFIGURATION .. && make -j 2 && ./rwe_test" || goto :error
+C:\msys64\usr\bin\bash -lc "cd $APPVEYOR_BUILD_FOLDER && mkdir build && cd build && cmake -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE=$Configuration .. && make -j 2 && ./rwe_test" || goto :error
 
 rem Create the build artifacts
 C:\msys64\usr\bin\bash -lc "cd $APPVEYOR_BUILD_FOLDER/build && make -j 2 package" || goto :error
