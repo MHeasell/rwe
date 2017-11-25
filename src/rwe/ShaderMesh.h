784250bdd2cf5b5bd4139313bb1c037f009105c3
Michael Heasell <michael.heasell@gmail.com>
2017-11-25 22:35:57 +0000

    Reorder include

--- src/rwe/ShaderMesh.h
+++ src/rwe/ShaderMesh.h
@@ -1,10 +1,10 @@
 #ifndef RWE_SHADERMESH_H
 #define RWE_SHADERMESH_H
 
+#include "GlMesh.h"
 #include "TextureHandle.h"
 #include "VaoHandle.h"
 #include "VboHandle.h"
-#include "GlMesh.h"
 
 namespace rwe
 {
