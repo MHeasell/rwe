0892a844c1155c4fcf6e8bc9ecafdcad82371a20
Michael Heasell <michael.heasell@gmail.com>
2017-12-03 22:13:15 +0000

    Clamp textures to edge

--- src/rwe/GraphicsContext.cpp
+++ src/rwe/GraphicsContext.cpp
@@ -78,6 +78,9 @@ namespace rwe
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
 
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
         return handle;
     }
 
