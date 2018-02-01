b79bf80738c4d345aa4d9db615575000b96b49fe
Michael Heasell <michael.heasell@gmail.com>
2018-02-01 22:14:05 +0000

    Implement movement class slope checks accurately
    Tested with the ARM Triton on Evad River Confluence.
    The visualisation appears to match exactly
    with the one from the TA debug view.

--- src/rwe/MovementClassCollisionService.cpp
+++ src/rwe/MovementClassCollisionService.cpp
@@ -84,7 +84,7 @@ namespace rwe
     bool
     isGridPointWalkable(const MapTerrain& terrain, const MovementClass& movementClass, unsigned int x, unsigned int y)
     {
-        if (isMaxSlopeGreaterThan(terrain.getHeightMap(), x, y, movementClass.footprintX, movementClass.footprintZ, movementClass.maxSlope))
+        if (isMaxSlopeGreaterThan(terrain.getHeightMap(), terrain.getSeaLevel(), x, y, movementClass.footprintX, movementClass.footprintZ, movementClass.maxSlope, movementClass.maxWaterSlope))
         {
             return false;
         }
@@ -97,34 +97,18 @@ namespace rwe
         return true;
     }
 
-    bool isMaxSlopeGreaterThan(const Grid<unsigned char>& heights, unsigned int x, unsigned int y, unsigned int width, unsigned int height, unsigned int maxSlope)
+    bool isMaxSlopeGreaterThan(const Grid<unsigned char>& heights, unsigned int waterLevel, unsigned int x, unsigned int y, unsigned int width, unsigned int height, unsigned int maxSlope, unsigned int maxWaterSlope)
     {
-        unsigned int minHeight = heights.get(x, y);
-        unsigned int maxHeight = heights.get(x, y);
+        auto isUnderWater = isAreaUnderWater(heights, waterLevel, x, y, width, height);
+        auto effectiveMaxSlope = isUnderWater ? maxWaterSlope : maxSlope;
 
         for (unsigned int dy = 0; dy < height; ++dy)
         {
             for (unsigned int dx = 0; dx < width; ++dx)
             {
-                auto cellHeight = heights.get(x + dx, y + dy);
-                if (cellHeight < minHeight)
+                if (getSlope(heights, x + dx, y + dy) > effectiveMaxSlope)
                 {
-                    minHeight = cellHeight;
-                    auto difference = maxHeight - minHeight;
-                    if (difference > maxSlope)
-                    {
-                        return true;
-                    }
-                }
-
-                if (cellHeight > maxHeight)
-                {
-                    maxHeight = cellHeight;
-                    auto difference = maxHeight - minHeight;
-                    if (difference > maxSlope)
-                    {
-                        return true;
-                    }
+                    return true;
                 }
             }
         }
@@ -135,30 +119,19 @@ namespace rwe
     bool
     isWaterDepthWithinBounds(const Grid<unsigned char>& heights, unsigned int waterLevel, unsigned int x, unsigned int y, unsigned int width, unsigned int height, unsigned int minWaterDepth, unsigned int maxWaterDepth)
     {
-        unsigned int minFoundWaterDepth = getWaterDepth(heights, waterLevel, x, y);
-        unsigned int maxFoundWaterDepth = getWaterDepth(heights, waterLevel, x, y);
-
         for (unsigned int dy = 0; dy < height; ++dy)
         {
             for (unsigned int dx = 0; dx < width; ++dx)
             {
                 auto cellWaterDepth = getWaterDepth(heights, waterLevel, x + dx, y + dy);
-                if (cellWaterDepth < minFoundWaterDepth)
+                if (cellWaterDepth < minWaterDepth)
                 {
-                    minFoundWaterDepth = cellWaterDepth;
-                    if (minFoundWaterDepth < minWaterDepth)
-                    {
-                        return false;
-                    }
+                    return false;
                 }
 
-                if (cellWaterDepth > maxFoundWaterDepth)
+                if (cellWaterDepth > maxWaterDepth)
                 {
-                    maxFoundWaterDepth = cellWaterDepth;
-                    if (maxFoundWaterDepth > maxWaterDepth)
-                    {
-                        return false;
-                    }
+                    return false;
                 }
             }
         }
@@ -171,5 +144,55 @@ namespace rwe
     {
         auto height = heights.get(x, y);
         return height < waterLevel ? waterLevel - height : 0;
+    }
+
+    unsigned int getSlope(const Grid<unsigned char>& heights, unsigned int x, unsigned int y)
+    {
+        unsigned int minHeight = 255;
+        unsigned int maxHeight = 0;
+
+        for (unsigned int dy = 0; dy < 2; ++dy)
+        {
+            for (unsigned int dx = 0; dx < 2; ++dx)
+            {
+                auto cellHeight = heights.get(x + dx, y + dy);
+
+                if (cellHeight < minHeight)
+                {
+                    minHeight = cellHeight;
+                }
+
+                if (cellHeight > maxHeight)
+                {
+                    maxHeight = cellHeight;
+                }
+            }
+        }
+
+        auto slope = maxHeight - minHeight;
+        return slope;
+    }
+
+    bool isAreaUnderWater(
+        const Grid<unsigned char>& heights,
+        unsigned int waterLevel,
+        unsigned int x,
+        unsigned int y,
+        unsigned int width,
+        unsigned int height)
+    {
+        for (unsigned int dy = 0; dy < height + 1; ++dy)
+        {
+            for (unsigned int dx = 0; dx < width + 1; ++dx)
+            {
+                auto cellHeight = heights.get(x + dx, y + dy);
+                if (cellHeight < waterLevel)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
