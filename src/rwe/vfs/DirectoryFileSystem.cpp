7f0960abbe1d6268e45204f65777bfa9a285f58a
Michael Heasell <michael.heasell@gmail.com>
2018-06-15 13:04:11 +0100

    Fix issues with plain directories on case-sensitive filesystems

--- src/rwe/vfs/DirectoryFileSystem.cpp
+++ src/rwe/vfs/DirectoryFileSystem.cpp
@@ -8,6 +8,45 @@ namespace fs = boost::filesystem;
 
 namespace rwe
 {
+    /**
+     * Searches the directory tree for the given path
+     * in a case-insensitive manner and returns the path with the correct case.
+     * The search starts from the root, which is assumed
+     * to already be correctly cased.
+     *
+     * This function is necessary because file references in TA
+     * are case-insensitive, however some of the filesystems
+     * we may be operating on (e.g. Linux) are case-sensitive.
+     * If we naively try to follow file references on a case-sensitive filesystem
+     * we may fail to find the file we wanted.
+     */
+    std::optional<boost::filesystem::path> findPathCaseInsensitive(
+        const boost::filesystem::path& root, const boost::filesystem::path& path)
+    {
+        fs::path basePath;
+
+        for (const auto& component : path)
+        {
+            fs::path searchPath(root);
+            searchPath /= basePath;
+            fs::directory_iterator it(searchPath);
+            fs::directory_iterator end;
+
+            auto foundItem = std::find_if(it, end, [&component](const auto& e) {
+                return toUpper(e.path().filename().string()) == toUpper(component.string());
+            });
+
+            if (foundItem == end)
+            {
+                return std::nullopt;
+            }
+
+            basePath /= foundItem->path().filename();
+        }
+
+        return basePath;
+    }
+
     DirectoryFileSystem::DirectoryFileSystem(const std::string& path)
         : path(path)
     {
@@ -20,9 +59,14 @@ namespace rwe
 
     std::optional<std::vector<char>> DirectoryFileSystem::readFile(const std::string& filename) const
     {
-        fs::path fullPath;
-        fullPath /= path;
-        fullPath /= filename;
+        fs::path fullPath(path);
+        auto correctlyCasedPath = findPathCaseInsensitive(path, filename);
+        if (!correctlyCasedPath)
+        {
+            return std::nullopt;
+        }
+
+        fullPath /= *correctlyCasedPath;
 
         std::ifstream input(fullPath.string(), std::ios::binary);
         if (!input.is_open())
