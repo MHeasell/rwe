8f7297f59038d4d45e16b778f1fb3b679cace327
Michael Heasell <michael.heasell@gmail.com>
2018-08-19 20:39:13 +0100

    Extract middleware into a file

--- launcher/src/renderer.tsx
+++ launcher/src/renderer.tsx
@@ -6,76 +6,18 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { applyMiddleware, createStore, Store, compose, MiddlewareAPI, Middleware, Dispatch } from "redux";
+import { applyMiddleware, createStore, Store, compose } from "redux";
 import thunk from "redux-thunk";
-import { receiveRooms, AppAction, startGameThunk } from "./actions";
+import { receiveRooms } from "./actions";
 import App from "./components/App";
 import rootReducer from "./reducers";
-import { State } from "./state";
 import { getRooms } from "./web";
+import { gameRoomMiddleware } from "./middleware/GameRoomMiddleware"
 
 import "./style.css";
-import { GameHostService } from "./ws/game-server";
-import { GameClientService } from "./ws/game-client";
-
-function looksLikeIPv6Address(value: string) {
-  return value.match(/^[0-9a-fA-F:]+$/) && value.match(/:/);
-}
-
-const gameHoster: Middleware = (store: MiddlewareAPI<Dispatch, State>) => {
-  const hostService = new GameHostService(store);
-  const clientService = new GameClientService(store);
-  return (next: (action: AppAction) => void) => (action: AppAction) => {
-    next(action);
-
-    switch (action.type) {
-      case "HOST_GAME_FORM_CONFIRM": {
-        hostService.createServer(action.gameDescription, action.players, 1337);
-        clientService.connectToServer("http://localhost:1337/", action.playerName);
-        break;
-      }
-      case "JOIN_SELECTED_GAME_CONFIRM": {
-        const state = store.getState();
-        if (state.selectedGameId === undefined) { break; }
-        const gameInfo = state.games.find(x => x.id == state.selectedGameId)!;
-        // hackily detect IPv6 addressses and enclose them in []
-        let address = looksLikeIPv6Address(gameInfo.host) ? `[${gameInfo.host}]` : gameInfo.host;
-        const connectionString = `http://${address}:${gameInfo.port}/`;
-        console.log(`connecting to ${connectionString}`);
-        clientService.connectToServer(connectionString, action.name);
-        break;
-      }
-      case "SEND_CHAT_MESSAGE": {
-        clientService.sendChatMessage(action.message);
-        break;
-      }
-      case "TOGGLE_READY": {
-        const state = store.getState();
-        if (!state.currentGame) { break; }
-        if (state.currentGame.localPlayerId === undefined) { break; }
-        const currentValue = state.currentGame.players.find(x => x.id === state.currentGame!.localPlayerId)!.ready;
-        clientService.setReadyState(!currentValue);
-        break;
-      }
-      case "LEAVE_GAME": {
-        clientService.disconnect();
-        hostService.destroyServer();
-        break;
-      }
-      case "SEND_START_GAME": {
-        clientService.requestStartGame();
-        break;
-      }
-      case "RECEIVE_START_GAME": {
-        store.dispatch<any>(startGameThunk());
-        break;
-      }
-    }
-  };
-};
 
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store: Store<any> = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, gameHoster)));
+const store: Store<any> = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, gameRoomMiddleware)));
 
 function doPollLoop() {
   getRooms()
