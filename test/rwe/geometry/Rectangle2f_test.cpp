7ee36d2652a275420f36a62c05bcecea9116bc90
Michael Heasell <michael.heasell@gmail.com>
2018-11-16 22:44:46 +0000

    Fix wrong method name in test

--- test/rwe/geometry/Rectangle2f_test.cpp
+++ test/rwe/geometry/Rectangle2f_test.cpp
@@ -135,7 +135,7 @@ namespace rwe
             }
         }
 
-        SECTION(".shrink")
+        SECTION(".scaleToFit")
         {
             SECTION("preserves identical rectangles")
             {
