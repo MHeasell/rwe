c8b2c3ee5ffb7007bc05f5c0dafc748c280052b9
Michael Heasell <michael.heasell@gmail.com>
2019-07-27 21:38:53 +0100

    Add VectorMap
    This is effectively an object pool backed by a deque.
    New objects are created by adding to the end of the deque.
    Objects that are destroyed leave holes
    which are tracked and reused for new objects.
    IDs contain a generation number
    so that replacement objects filling the holes
    are not conflated with the previous object that was there.

---
+++ src/rwe/VectorMap.h
@@ -0,0 +1,258 @@
+#ifndef RWE_VECTORMAP_H
+#define RWE_VECTORMAP_H
+
+#include <cassert>
+#include <deque>
+#include <functional>
+#include <rwe/OpaqueId.h>
+#include <rwe/overloaded.h>
+#include <utility>
+#include <variant>
+
+namespace rwe
+{
+    template <typename T, typename IdTag>
+    class VectorMap
+    {
+    private:
+        using Id = OpaqueId<unsigned int, IdTag>;
+        struct IndexTag;
+        using Index = OpaqueId<unsigned int, IndexTag>;
+        struct GenerationTag;
+        using Generation = OpaqueId<unsigned int, GenerationTag>;
+
+        struct FreeEntry
+        {
+            std::optional<Index> nextIndex;
+            Id id;
+            FreeEntry(Id id, const std::optional<Index>& nextIndex) : nextIndex(nextIndex), id(id) {}
+        };
+
+        using OccupiedEntry = std::pair<Id, T>;
+        using Entry = std::variant<FreeEntry, OccupiedEntry>;
+
+    public:
+        template <typename Val, typename Ref, typename Ptr, typename UnderlyingIt>
+        class VectorMapIter
+        {
+        public:
+            using iterator_category = std::forward_iterator_tag;
+            using value_type = Val;
+            using reference = Ref;
+            using pointer = Ptr;
+            using difference_type = int; // not used
+
+        private:
+            UnderlyingIt it;
+            UnderlyingIt end;
+
+        public:
+            VectorMapIter(
+                UnderlyingIt it,
+                UnderlyingIt end) : it(std::move(it)), end(std::move(end))
+            {
+            }
+
+            VectorMapIter& operator++()
+            {
+                assert(it != end);
+                it = firstValid(++it, end);
+                return *this;
+            }
+
+            VectorMapIter operator++(int)
+            {
+                VectorMapIter tmp(*this);
+                ++*this;
+                return tmp;
+            }
+
+            reference operator*() const
+            {
+                return std::get<std::remove_const_t<value_type>>(*it);
+            }
+
+            pointer operator->() const
+            {
+                return &std::get<std::remove_const_t<value_type>>(*it);
+            }
+
+            bool operator==(const VectorMapIter& rhs) const
+            {
+                return it == rhs.it && end == rhs.end;
+            }
+
+            bool operator!=(const VectorMapIter& rhs) const
+            {
+                return !(rhs == *this);
+            }
+        };
+
+        using iterator = VectorMapIter<OccupiedEntry, OccupiedEntry&, OccupiedEntry*, typename std::deque<Entry>::iterator>;
+        using const_iterator = VectorMapIter<const OccupiedEntry, const OccupiedEntry&, const OccupiedEntry*, typename std::deque<Entry>::const_iterator>;
+
+        using key_type = Id;
+        using mapped_type = T;
+        using value_type = OccupiedEntry;
+
+    private:
+        std::deque<Entry> vec;
+        std::optional<Index> firstFreeSlotIndex;
+
+    public:
+        template <typename... Args>
+        Id emplace(Args&&... args)
+        {
+            if (firstFreeSlotIndex)
+            {
+                auto index = *firstFreeSlotIndex;
+                auto& entry = vec[index.value];
+                const auto& freeEntry = std::get<FreeEntry>(vec[index.value]);
+                firstFreeSlotIndex = freeEntry.nextIndex;
+                auto newId = nextGeneration(freeEntry.id);
+                entry = std::make_pair(newId, T(std::forward<Args>(args)...));
+                return newId;
+            }
+            else
+            {
+                auto id = makeId(Index(vec.size()));
+                vec.emplace_back(std::make_pair(id, T(std::forward<Args>(args)...)));
+                return id;
+            }
+        }
+
+        void remove(Id id)
+        {
+            auto [index, generation] = parseId(id);
+
+            if (index.value >= vec.size())
+            {
+                throw std::runtime_error("Member with given ID does not exist");
+            }
+
+            auto& entry = vec[index.value];
+            if (const auto f = std::get_if<OccupiedEntry>(&entry); f != nullptr && f->first == id)
+            {
+                entry = FreeEntry(id, firstFreeSlotIndex);
+                firstFreeSlotIndex = index;
+                return;
+            }
+
+            throw std::runtime_error("Member with given ID does not exist");
+        }
+
+        std::optional<std::reference_wrapper<T>> tryGet(Id id)
+        {
+            auto [index, _] = parseId(id);
+
+            auto& entry = vec[index.value];
+            return match(
+                entry,
+                [](const FreeEntry&) { return std::optional<std::reference_wrapper<T>>(); },
+                [id](OccupiedEntry& e) {
+                    return e.first == id
+                        ? std::make_optional(std::ref(e.second))
+                        : std::optional<std::reference_wrapper<T>>();
+                });
+        }
+
+        std::optional<std::reference_wrapper<const T>> tryGet(Id id) const
+        {
+            auto [index, _] = parseId(id);
+
+            const auto& entry = vec[index.value];
+            return match(
+                entry,
+                [](const FreeEntry&) { return std::optional<std::reference_wrapper<const T>>(); },
+                [id](const OccupiedEntry& e) {
+                    return e.first == id
+                        ? std::make_optional(std::ref(e.second))
+                        : std::optional<std::reference_wrapper<const T>>();
+                });
+        }
+
+        iterator begin()
+        {
+            return iterator(firstValid(vec.begin(), vec.end()), vec.end());
+        }
+
+        iterator end()
+        {
+            return iterator(vec.end(), vec.end());
+        }
+
+        const_iterator begin() const
+        {
+            return const_iterator(firstValid(vec.begin(), vec.end()), vec.end());
+        }
+
+        const_iterator end() const
+        {
+            return const_iterator(vec.end(), vec.end());
+        }
+
+        iterator erase(iterator it)
+        {
+            remove(it->first);
+            return ++it;
+        }
+
+        const_iterator find(Id id) const
+        {
+            auto [index, generation] = parseId(id);
+            if (index.value >= vec.size())
+            {
+                return end();
+            }
+
+            auto it = vec.begin() + index.value;
+            return match(
+                *it,
+                [&](const FreeEntry&) { return end(); },
+                [&](const OccupiedEntry& e) { return e.first == id ? const_iterator(it, vec.end()) : end(); });
+        }
+
+        iterator find(Id id)
+        {
+            auto [index, generation] = parseId(id);
+            if (index.value >= vec.size())
+            {
+                return end();
+            }
+
+            auto it = vec.begin() + index.value;
+            return match(
+                *it,
+                [&](const FreeEntry&) { return end(); },
+                [&](OccupiedEntry& e) { return e.first == id ? iterator(it, vec.end()) : end(); });
+        }
+
+    private:
+        static std::pair<Index, Generation> parseId(Id id)
+        {
+            return std::make_pair(Index(id.value >> 8u), Generation(id.value & 0xFFu));
+        }
+        static Id makeId(Index index)
+        {
+            return Id(index.value << 8u);
+        }
+        static Id makeId(Index index, Generation generation)
+        {
+            return Id((index.value << 8u) | generation.value);
+        }
+
+        static Id nextGeneration(Id id)
+        {
+            auto [index, generation] = parseId(id);
+            return makeId(index, Generation((generation.value + 1) & 0xFFu));
+        }
+
+        template <typename It>
+        static It firstValid(It it, It end)
+        {
+            return std::find_if(it, end, [](const auto& v) { return std::holds_alternative<OccupiedEntry>(v); });
+        }
+    };
+}
+
+#endif
---
+++ test/rwe/VectorMap_test.cpp
@@ -0,0 +1,285 @@
+#include <catch2/catch.hpp>
+#include <rwe/OpaqueId.h>
+#include <rwe/VectorMap.h>
+#include <rwe/optional_io.h>
+
+namespace rwe
+{
+    struct IdTag;
+    using Id = OpaqueId<unsigned int, IdTag>;
+    TEST_CASE("VectorMap")
+    {
+        SECTION("can store and retrieve elements")
+        {
+            VectorMap<char, IdTag> m;
+
+            auto idA = m.emplace('a');
+            auto idB = m.emplace('b');
+            auto idC = m.emplace('c');
+            REQUIRE(idA == Id(0));
+            REQUIRE(idB == Id(256));
+            REQUIRE(idC == Id(512));
+
+            REQUIRE(m.tryGet(idA) == 'a');
+            REQUIRE(m.tryGet(idB) == 'b');
+            REQUIRE(m.tryGet(idC) == 'c');
+        }
+
+        SECTION("works with const")
+        {
+            VectorMap<int, IdTag> m;
+            const auto& c = m;
+
+            auto id = m.emplace(1);
+
+            auto elem = c.tryGet(id);
+            REQUIRE(elem == 1);
+        }
+
+        SECTION("allows removing elements")
+        {
+            VectorMap<char, IdTag> m;
+            auto idA = m.emplace('a');
+            REQUIRE(m.tryGet(idA) == 'a');
+            m.remove(idA);
+            REQUIRE(m.tryGet(idA) == std::nullopt);
+        }
+
+        SECTION("allows removing elements and filling in holes")
+        {
+            VectorMap<char, IdTag> m;
+
+            // add some elements
+            auto idA = m.emplace('a');
+            auto idB = m.emplace('b');
+            auto idC = m.emplace('c');
+            auto idD = m.emplace('d');
+            auto idE = m.emplace('e');
+
+            // delete some (make some holes)
+            m.remove(idD);
+            m.remove(idB);
+
+            // add new elements to fill in the holes
+            auto idF = m.emplace('f');
+            auto idG = m.emplace('g');
+            auto idH = m.emplace('h');
+
+            REQUIRE(idA == Id(0));
+            REQUIRE(idB == Id(256));
+            REQUIRE(idC == Id(512));
+            REQUIRE(idD == Id(768));
+            REQUIRE(idE == Id(1024));
+
+            REQUIRE(idF == Id(257)); // idB + 1 generation
+            REQUIRE(idG == Id(769)); // idD + 1 generation
+            REQUIRE(idH == Id(1280)); // new slot
+
+            REQUIRE(m.tryGet(idA) == 'a');
+            REQUIRE(m.tryGet(idB) == std::nullopt);
+            REQUIRE(m.tryGet(idC) == 'c');
+            REQUIRE(m.tryGet(idD) == std::nullopt);
+            REQUIRE(m.tryGet(idE) == 'e');
+            REQUIRE(m.tryGet(idF) == 'f');
+            REQUIRE(m.tryGet(idG) == 'g');
+            REQUIRE(m.tryGet(idH) == 'h');
+
+            m.remove(idG);
+            auto idI = m.emplace('i');
+            REQUIRE(idI == Id(770)); // idD + 2 generations
+            REQUIRE(m.tryGet(idG) == std::nullopt);
+            REQUIRE(m.tryGet(idI) == 'i');
+        }
+
+        SECTION("generations wrap around to 0")
+        {
+            VectorMap<char, IdTag> m;
+            for (int i = 0; i < 255; ++i)
+            {
+                auto id = m.emplace('x');
+                m.remove(id);
+            }
+
+            auto id = m.emplace('a');
+            REQUIRE(id == Id(255));
+            m.remove(id);
+            auto id2 = m.emplace('a');
+            REQUIRE(id2 == Id(0));
+            REQUIRE(m.tryGet(id2) == 'a');
+        }
+
+        SECTION("supports iteration")
+        {
+            VectorMap<char, IdTag> m;
+            m.emplace('a');
+            m.emplace('b');
+            m.emplace('c');
+            m.emplace('d');
+            m.emplace('e');
+
+            std::vector<std::pair<Id, char>> items{{Id(0), 'a'}, {Id(256), 'b'}, {Id(512), 'c'}, {Id(768), 'd'}, {Id(1024), 'e'}};
+            int i = 0;
+            for (const auto& p : m)
+            {
+                REQUIRE(p == items.at(i));
+                ++i;
+            }
+        }
+
+        SECTION("supports const iteration")
+        {
+            VectorMap<char, IdTag> m;
+            m.emplace('a');
+            m.emplace('b');
+            m.emplace('c');
+            m.emplace('d');
+            m.emplace('e');
+
+            const auto& x = m;
+
+            std::vector<std::pair<Id, char>> items{{Id(0), 'a'}, {Id(256), 'b'}, {Id(512), 'c'}, {Id(768), 'd'}, {Id(1024), 'e'}};
+            int i = 0;
+            for (const auto& p : x)
+            {
+                REQUIRE(p == items.at(i));
+                ++i;
+            }
+        }
+
+        SECTION("iteration skips holes at end")
+        {
+            VectorMap<char, IdTag> m;
+            m.emplace('a');
+            m.emplace('b');
+            m.emplace('c');
+            auto idD = m.emplace('d');
+            auto idE = m.emplace('e');
+
+            m.remove(idD);
+            m.remove(idE);
+
+            std::vector<std::pair<Id, char>> items{{Id(0), 'a'}, {Id(256), 'b'}, {Id(512), 'c'}};
+            int i = 0;
+            for (const auto& p : m)
+            {
+                REQUIRE(p == items.at(i));
+                ++i;
+            }
+        }
+
+        SECTION("iteration skips holes at beginning")
+        {
+            VectorMap<char, IdTag> m;
+            auto idA = m.emplace('a');
+            auto idB = m.emplace('b');
+            m.emplace('c');
+            m.emplace('d');
+            m.emplace('e');
+
+            m.remove(idA);
+            m.remove(idB);
+
+            std::vector<std::pair<Id, char>> items{{Id(512), 'c'}, {Id(768), 'd'}, {Id(1024), 'e'}};
+            int i = 0;
+            for (const auto& p : m)
+            {
+                REQUIRE(p == items.at(i));
+                ++i;
+            }
+        }
+
+        SECTION("iteration skips holes in the middle")
+        {
+            VectorMap<char, IdTag> m;
+            m.emplace('a');
+            m.emplace('b');
+            auto idC = m.emplace('c');
+            m.emplace('d');
+            auto idE = m.emplace('e');
+            auto idF = m.emplace('f');
+            m.emplace('g');
+
+            m.remove(idC);
+            m.remove(idE);
+            m.remove(idF);
+
+            std::vector<std::pair<Id, char>> items{{Id(0), 'a'}, {Id(256), 'b'}, {Id(768), 'd'}, {Id(1536), 'g'}};
+            int i = 0;
+            for (const auto& p : m)
+            {
+                REQUIRE(p == items.at(i));
+                ++i;
+            }
+        }
+
+        SECTION("supports erasing during iteration")
+        {
+            VectorMap<char, IdTag> m;
+            m.emplace('a');
+            m.emplace('b');
+            m.emplace('c');
+            m.emplace('d');
+            m.emplace('e');
+
+            auto it = std::find_if(m.begin(), m.end(), [](const auto& p) { return p.second == 'c'; });
+            it = m.erase(it);
+
+            REQUIRE(it->second == 'd');
+        }
+
+        SECTION("erasing skips holes after")
+        {
+            VectorMap<char, IdTag> m;
+            m.emplace('a');
+            m.emplace('b');
+            m.emplace('c');
+            auto dId = m.emplace('d');
+            m.emplace('e');
+
+            m.remove(dId);
+
+            auto it = std::find_if(m.begin(), m.end(), [](const auto& p) { return p.second == 'c'; });
+            it = m.erase(it);
+
+            REQUIRE(it->second == 'e');
+        }
+
+        SECTION("supports find")
+        {
+            VectorMap<char, IdTag> m;
+            m.emplace('a');
+            m.emplace('b');
+            m.emplace('c');
+            auto dId = m.emplace('d');
+            m.emplace('e');
+
+            auto it = m.find(dId);
+            REQUIRE(it->first == Id(768));
+            REQUIRE(it->second == 'd');
+
+            m.remove(dId);
+
+            REQUIRE(m.find(dId) == m.end());
+        }
+
+        SECTION("find works with const")
+        {
+            VectorMap<char, IdTag> m;
+            m.emplace('a');
+            m.emplace('b');
+            m.emplace('c');
+            auto dId = m.emplace('d');
+            m.emplace('e');
+
+            const auto& c = m;
+
+            auto it = c.find(dId);
+            REQUIRE(it->first == Id(768));
+            REQUIRE(it->second == 'd');
+
+            m.remove(dId);
+
+            REQUIRE(c.find(dId) == c.end());
+        }
+    }
+}
