93020fb63adfb91a28b3d97b1eb8a02847e8140b
Michael Heasell <michael.heasell@gmail.com>
2019-08-25 14:45:26 +0100

    Delete unused method Plane3f::intersectOrInfinity

--- src/rwe/geometry/Plane3f.h
+++ src/rwe/geometry/Plane3f.h
@@ -1,21 +1,8 @@
 #pragma once
 
 #include <rwe/geometry/Plane3x.h>
-#include <rwe/geometry/Ray3f.h>
 
 namespace rwe
 {
     using Plane3f = Plane3x<float>;
-
-    /**
-     * Returns the distance along the ray
-     * at which it intersects with this plane.
-     * The distance is defined in terms of the ray's
-     * direction vector.
-     *
-     * If the ray does not intersect, i.e. it is parallel,
-     * if the ray is in front of the plane we return infinity,
-     * otherwise we return negative infinity.
-     */
-    float intersectOrInfinity(const Plane3f& plane, const Ray3f& ray);
 }
