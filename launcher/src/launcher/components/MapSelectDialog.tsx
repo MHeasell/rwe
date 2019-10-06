339a36509f60b9d5923d7d8648f5b337d7988566
Michael Heasell <michael.heasell@gmail.com>
2019-10-06 02:16:28 +0100

    Convert maps dialog to a function using hooks

--- launcher/src/launcher/components/MapSelectDialog.tsx
+++ launcher/src/launcher/components/MapSelectDialog.tsx
@@ -36,142 +36,110 @@ export interface MapSelectDialogProps extends WithStyles<typeof styles> {
   onClose: () => void;
 }
 
-class MapSelectDialog extends React.Component<MapSelectDialogProps> {
-  private readonly listRef: React.RefObject<List>;
+function MapSelectDialog(props: MapSelectDialogProps) {
+  const listRef = React.useRef<List>(null);
 
-  constructor(props: MapSelectDialogProps) {
-    super(props);
-    this.listRef = React.createRef();
-  }
-
-  componentDidMount() {
-    this.scrollToSelectedItem();
-  }
-
-  componentDidUpdate(prevProps: MapSelectDialogProps) {
-    if (!prevProps.maps && this.props.maps) {
-      this.scrollToSelectedItem();
+  React.useEffect(() => {
+    if (!props.maps || !props.selectedMap || !listRef.current) {
+      return;
     }
-  }
+    const idx = props.maps!.indexOf(props.selectedMap);
+    if (idx === -1) {
+      return;
+    }
+    listRef.current.scrollToItem(idx, "center");
+  }, [!!props.maps]);
 
-  render() {
-    const mapImage = this.props.minimapSrc ? (
-      <img
-        src={`file://${this.props.minimapSrc}`}
-        className="map-dialog-minimap"
-      />
-    ) : (
-      <React.Fragment />
-    );
+  const mapImage = props.minimapSrc ? (
+    <img src={`file://${props.minimapSrc}`} className="map-dialog-minimap" />
+  ) : (
+    <React.Fragment />
+  );
 
-    const mapDescription = this.props.selectedMapDetails
-      ? this.props.selectedMapDetails.description
-      : "";
-    const mapDescription2 = this.props.selectedMapDetails
-      ? `${this.props.selectedMapDetails.memory} Players: ${this.props.selectedMapDetails.numberOfPlayers}`
-      : "";
+  const mapDescription = props.selectedMapDetails
+    ? props.selectedMapDetails.description
+    : "";
+  const mapDescription2 = props.selectedMapDetails
+    ? `${props.selectedMapDetails.memory} Players: ${props.selectedMapDetails.numberOfPlayers}`
+    : "";
 
-    return (
-      <Dialog open={this.props.open} onClose={this.props.onClose} fullWidth>
-        <DialogTitle>Select Map</DialogTitle>
-        <DialogContent>
-          <div className="map-dialog-container">
-            <div className="map-dialog-left">
-              <div className="map-dialog-list">{this.renderMaps()}</div>
-            </div>
-            <div className="map-dialog-right">
-              <div className="map-dialog-minimap-container">{mapImage}</div>
-              <div className="map-dialog-map-info">
-                <Typography>{mapDescription}</Typography>
-                <Typography>{mapDescription2}</Typography>
-              </div>
+  return (
+    <Dialog open={props.open} onClose={props.onClose} fullWidth>
+      <DialogTitle>Select Map</DialogTitle>
+      <DialogContent>
+        <div className="map-dialog-container">
+          <div className="map-dialog-left">
+            <div className="map-dialog-list">
+              <MapsList
+                maps={props.maps}
+                selectedMap={props.selectedMap}
+                onSelect={props.onSelect}
+                listRef={listRef}
+              />
             </div>
           </div>
-        </DialogContent>
-        <DialogActions>
-          <Button color="primary" onClick={this.props.onConfirm}>
-            Select Map
-          </Button>
-          <Button onClick={this.props.onClose}>Back</Button>
-        </DialogActions>
-      </Dialog>
-    );
+          <div className="map-dialog-right">
+            <div className="map-dialog-minimap-container">{mapImage}</div>
+            <div className="map-dialog-map-info">
+              <Typography>{mapDescription}</Typography>
+              <Typography>{mapDescription2}</Typography>
+            </div>
+          </div>
+        </div>
+      </DialogContent>
+      <DialogActions>
+        <Button color="primary" onClick={props.onConfirm}>
+          Select Map
+        </Button>
+        <Button onClick={props.onClose}>Back</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+function MapsList(props: {
+  maps?: string[];
+  selectedMap?: string;
+  onSelect: (name: string) => void;
+  listRef: React.RefObject<List>;
+}) {
+  if (!props.maps) {
+    return <Typography>Maps not yet loaded</Typography>;
   }
 
-  private renderMaps() {
-    if (!this.props.maps) {
-      return <Typography>Maps not yet loaded</Typography>;
-    }
-
-    const Row = ({
-      index,
-      style,
-    }: {
-      index: number;
-      style: React.CSSProperties;
-    }) =>
-      this.toMapDialogItem(
-        this.props.maps![index],
-        this.props.selectedMap,
-        style
-      );
-
-    return (
-      <List
-        ref={this.listRef}
-        height={320}
-        width={292}
-        itemSize={35}
-        itemCount={this.props.maps.length}
-      >
-        {Row}
-      </List>
-    );
-  }
-
-  private toMapDialogItem(
-    name: string,
-    selectedMap: string | undefined,
-    style: any
-  ) {
-    if (name === selectedMap) {
-      return (
-        <ListItem
-          key={name}
-          dense
-          style={style}
-          button
-          selected={name === selectedMap}
-          onClick={() => this.props.onSelect(name)}
-        >
-          <ListItemText primary={name}></ListItemText>
-        </ListItem>
-      );
-    }
+  const Row = ({
+    index,
+    style,
+  }: {
+    index: number;
+    style: React.CSSProperties;
+  }) => {
+    const name = props.maps![index];
     return (
       <ListItem
+        key={name}
         dense
         style={style}
         button
-        key={name}
-        selected={name === selectedMap}
-        onClick={() => this.props.onSelect(name)}
+        selected={name === props.selectedMap}
+        onClick={() => props.onSelect(name)}
       >
         <ListItemText primary={name}></ListItemText>
       </ListItem>
     );
-  }
+  };
 
-  private scrollToSelectedItem() {
-    if (!this.props.maps || !this.props.selectedMap || !this.listRef.current) {
-      return;
-    }
-    const idx = this.props.maps.indexOf(this.props.selectedMap);
-    if (idx === -1) {
-      return;
-    }
-    this.listRef.current.scrollToItem(idx, "center");
-  }
+  return (
+    <List
+      ref={props.listRef}
+      height={320}
+      width={292}
+      itemSize={35}
+      itemCount={props.maps.length}
+    >
+      {Row}
+    </List>
+  );
 }
 
 export default withStyles(styles)(MapSelectDialog);
