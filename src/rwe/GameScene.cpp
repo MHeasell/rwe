ceda762a32a47c18b6a2a47a42d0ed1158433ac4
Michael Heasell <michael.heasell@gmail.com>
2019-11-02 02:14:04 +0000

    Always clear unit selection on right cick in left click mode

--- src/rwe/GameScene.cpp
+++ src/rwe/GameScene.cpp
@@ -715,48 +715,55 @@ namespace rwe
                     cursorMode.next(NormalCursorMode());
                 },
                 [&](const NormalCursorMode&) {
-                    for (const auto& selectedUnit : selectedUnits)
+                    if (leftClickMode())
                     {
-                        if (hoveredUnit)
+                        clearUnitSelection();
+                    }
+                    else
+                    {
+                        for (const auto& selectedUnit : selectedUnits)
                         {
-                            if (isEnemy(*hoveredUnit))
+                            if (hoveredUnit)
                             {
-                                if (isShiftDown())
+                                if (isEnemy(*hoveredUnit))
                                 {
-                                    localPlayerEnqueueUnitOrder(selectedUnit, AttackOrder(*hoveredUnit));
+                                    if (isShiftDown())
+                                    {
+                                        localPlayerEnqueueUnitOrder(selectedUnit, AttackOrder(*hoveredUnit));
+                                    }
+                                    else
+                                    {
+                                        localPlayerIssueUnitOrder(selectedUnit, AttackOrder(*hoveredUnit));
+                                    }
                                 }
                                 else
                                 {
-                                    localPlayerIssueUnitOrder(selectedUnit, AttackOrder(*hoveredUnit));
+                                    if (getUnit(*hoveredUnit).isBeingBuilt())
+                                    {
+                                        if (isShiftDown())
+                                        {
+                                            localPlayerEnqueueUnitOrder(selectedUnit, CompleteBuildOrder(*hoveredUnit));
+                                        }
+                                        else
+                                        {
+                                            localPlayerIssueUnitOrder(selectedUnit, CompleteBuildOrder(*hoveredUnit));
+                                        }
+                                    }
                                 }
                             }
                             else
                             {
-                                if (getUnit(*hoveredUnit).isBeingBuilt())
+                                auto coord = getMouseTerrainCoordinate();
+                                if (coord)
                                 {
                                     if (isShiftDown())
                                     {
-                                        localPlayerEnqueueUnitOrder(selectedUnit, CompleteBuildOrder(*hoveredUnit));
+                                        localPlayerEnqueueUnitOrder(selectedUnit, MoveOrder(*coord));
                                     }
                                     else
                                     {
-                                        localPlayerIssueUnitOrder(selectedUnit, CompleteBuildOrder(*hoveredUnit));
+                                        localPlayerIssueUnitOrder(selectedUnit, MoveOrder(*coord));
                                     }
-                                }
-                            }
-                        }
-                        else
-                        {
-                            auto coord = getMouseTerrainCoordinate();
-                            if (coord)
-                            {
-                                if (isShiftDown())
-                                {
-                                    localPlayerEnqueueUnitOrder(selectedUnit, MoveOrder(*coord));
-                                }
-                                else
-                                {
-                                    localPlayerIssueUnitOrder(selectedUnit, MoveOrder(*coord));
                                 }
                             }
                         }
