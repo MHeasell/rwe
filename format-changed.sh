97a797bc77e20fabf434f627ca5f6b5126580f63
Michael Heasell <michael.heasell@gmail.com>
2020-05-25 01:36:59 +0100

    Add script to reformat only changed lines
    This is quicker to run than format.sh, useful during development.

---
+++ format-changed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+script_dir="$(readlink -f "$(dirname "$0")")"
+
+reformat() {
+    expand -t 4 "$1" | clang-format > "$1.__reformat-tmp"
+    mv "$1.__reformat-tmp" "$1"
+}
+
+git diff --name-only -z '*.cpp' '*.h' |
+while read -r -d '' file; do
+    "$script_dir/fix-includes.pl" "$file"
+    reformat "$file"
+done
+
+# Note: perl -i on cygwin always creates bak files.
+perl -i "$script_dir/fix-cmakelists.pl" "$script_dir/CMakeLists.txt" && rm -f "$script_dir/CMakeLists.txt.bak"
