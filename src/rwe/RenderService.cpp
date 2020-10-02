MHeasell/rwe

commit fba7d27d67459e67d75b75dc08aed575270c1137

Remove ShaderMesh from Unit + SelectionMesh cleanup

src/rwe/RenderService.cpp
@@ -27,11 +27,13 @@ namespace rwe
     RenderService::RenderService(
         GraphicsContext* graphics,
         ShaderService* shaders,
+        MeshDatabase&& meshDatabase,
         const CabinetCamera& camera,
         SharedTextureHandle unitTextureAtlas,
         std::vector<SharedTextureHandle>&& unitTeamTextureAtlases)
         : graphics(graphics),
           shaders(shaders),
+          meshDatabase(std::move(meshDatabase)),
           camera(camera),
           unitTextureAtlas(unitTextureAtlas),
           unitTeamTextureAtlases(std::move(unitTeamTextureAtlases))
@@ -39,8 +41,10 @@ namespace rwe
     }
 
     void
-    RenderService::drawSelectionRect(const Unit& unit, const SelectionMesh& selectionMesh)
+    RenderService::drawSelectionRect(const Unit& unit)
     {
+        auto selectionMesh = meshDatabase.getSelectionMesh(unit.objectName);
+
         // try to ensure that the selection rectangle vertices
         // are aligned with the middle of pixels,
         // to prevent discontinuities in the drawn lines.
@@ -55,7 +59,7 @@ namespace rwe
         graphics->bindShader(shader.handle.get());
         graphics->setUniformMatrix(shader.mvpMatrix, camera.getViewProjectionMatrix() * matrix);
         graphics->setUniformFloat(shader.alpha, 1.0f);
-        graphics->drawLineLoop(selectionMesh.visualMesh);
+        graphics->drawLineLoop(*selectionMesh.value());
     }
 
     void RenderService::drawNanolatheLine(const Vector3f& start, const Vector3f& end)
@@ -74,61 +78,43 @@ namespace rwe
     {
         if (unit.isBeingBuilt())
         {
-            drawBuildingUnitMesh(unit.mesh, toFloatMatrix(unit.getTransform()), seaLevel, unit.getPreciseCompletePercent(), simScalarToFloat(unit.position.y), time, playerColorIndex);
+            drawBuildingUnitMesh(unit.objectName, unit.mesh, toFloatMatrix(unit.getTransform()), seaLevel, unit.getPreciseCompletePercent(), simScalarToFloat(unit.position.y), time, playerColorIndex);
         }
         else
         {
-            drawUnitMesh(unit.mesh, toFloatMatrix(unit.getTransform()), seaLevel, playerColorIndex);
+            drawUnitMesh(unit.objectName, unit.mesh, toFloatMatrix(unit.getTransform()), seaLevel, playerColorIndex);
         }
     }
 
-    void RenderService::drawUnitMesh(const UnitMesh& mesh, const Matrix4f& modelMatrix, float seaLevel, PlayerColorIndex playerColorIndex)
+    void RenderService::drawUnitMesh(const std::string& objectName, const UnitMesh& mesh, const Matrix4f& modelMatrix, float seaLevel, PlayerColorIndex playerColorIndex)
     {
         auto matrix = modelMatrix * toFloatMatrix(mesh.getTransform());
 
         if (mesh.visible)
         {
-            drawShaderMesh(*mesh.mesh, matrix, seaLevel, mesh.shaded, playerColorIndex);
+            const auto& resolvedMesh = *meshDatabase.getUnitPieceMesh(objectName, mesh.name).value();
+            drawShaderMesh(resolvedMesh, matrix, seaLevel, mesh.shaded, playerColorIndex);
         }
 
         for (const auto& c : mesh.children)
         {
-            drawUnitMesh(c, matrix, seaLevel, playerColorIndex);
+            drawUnitMesh(objectName, c, matrix, seaLevel, playerColorIndex);
         }
     }
 
-    void RenderService::drawBuildingUnitMesh(const UnitMesh& mesh, const Matrix4f& modelMatrix, float seaLevel, float percentComplete, float unitY, float time, PlayerColorIndex playerColorIndex)
+    void RenderService::drawBuildingUnitMesh(const std::string& objectName, const UnitMesh& mesh, const Matrix4f& modelMatrix, float seaLevel, float percentComplete, float unitY, float time, PlayerColorIndex playerColorIndex)
     {
         auto matrix = modelMatrix * toFloatMatrix(mesh.getTransform());
 
         if (mesh.visible)
         {
-            auto mvpMatrix = camera.getViewProjectionMatrix() * matrix;
-
-            // TODO: draw colored vertices
-
-            {
-                const auto& buildShader = shaders->unitBuild;
-                graphics->bindShader(buildShader.handle.get());
-                graphics->setUniformMatrix(buildShader.mvpMatrix, mvpMatrix);
-                graphics->setUniformMatrix(buildShader.modelMatrix, matrix);
-                graphics->setUniformFloat(buildShader.unitY, unitY);
-                graphics->setUniformFloat(buildShader.seaLevel, seaLevel);
-                graphics->setUniformBool(buildShader.shade, mesh.shaded);
-                graphics->setUniformFloat(buildShader.percentComplete, percentComplete);
-                graphics->setUniformFloat(buildShader.time, time);
-
-                graphics->bindTexture(unitTextureAtlas.get());
-                graphics->drawTriangles(mesh.mesh->vertices);
-
-                graphics->bindTexture(unitTeamTextureAtlases.at(playerColorIndex.value).get());
-                graphics->drawTriangles(mesh.mesh->teamVertices);
-            }
+            const auto& resolvedMesh = *meshDatabase.getUnitPieceMesh(objectName, mesh.name).value();
+            drawBuildingShaderMesh(resolvedMesh, modelMatrix, seaLevel, mesh.shaded, percentComplete, unitY, time, playerColorIndex);
         }
 
         for (const auto& c : mesh.children)
         {
-            drawBuildingUnitMesh(c, matrix, seaLevel, percentComplete, unitY, time, playerColorIndex);
+            drawBuildingUnitMesh(objectName, c, matrix, seaLevel, percentComplete, unitY, time, playerColorIndex);
         }
     }
 
@@ -456,7 +442,7 @@ namespace rwe
 
         auto matrix = Matrix4f::translation(simVectorToFloat(unit.position)) * Matrix4f::rotationY(toRadians(unit.rotation).value);
 
-        drawUnitMesh(unit.mesh, shadowProjection * matrix, 0.0f, PlayerColorIndex(0));
+        drawUnitMesh(unit.objectName, unit.mesh, shadowProjection * matrix, 0.0f, PlayerColorIndex(0));
     }
 
     CabinetCamera& RenderService::getCamera()
@@ -549,7 +535,7 @@ namespace rwe
                     auto transform = Matrix4f::translation(position)
                         * pointDirection(simVectorToFloat(projectile.velocity).normalized())
                         * rotationModeToMatrix(m.rotationMode);
-                    drawUnitMesh(*m.mesh, transform, seaLevel, PlayerColorIndex(0));
+                    drawUnitMesh(m.objectName, *m.mesh, transform, seaLevel, PlayerColorIndex(0));
                 },
                 [&](const ProjectileRenderTypeSprite& s) {
                     Vector3f snappedPosition(
@@ -629,6 +615,29 @@ namespace rwe
             graphics->setUniformMatrix(textureShader.modelMatrix, matrix);
             graphics->setUniformFloat(textureShader.seaLevel, seaLevel);
             graphics->setUniformBool(textureShader.shade, shaded);
+
+            graphics->bindTexture(unitTextureAtlas.get());
+            graphics->drawTriangles(mesh.vertices);
+
+            graphics->bindTexture(unitTeamTextureAtlases.at(playerColorIndex.value).get());
+            graphics->drawTriangles(mesh.teamVertices);
+        }
+    }
+
+    void RenderService::drawBuildingShaderMesh(const ShaderMesh& mesh, const Matrix4f& matrix, float seaLevel, bool shaded, float percentComplete, float unitY, float time, PlayerColorIndex playerColorIndex)
+    {
+        auto mvpMatrix = camera.getViewProjectionMatrix() * matrix;
+
+        {
+            const auto& buildShader = shaders->unitBuild;
+            graphics->bindShader(buildShader.handle.get());
+            graphics->setUniformMatrix(buildShader.mvpMatrix, mvpMatrix);
+            graphics->setUniformMatrix(buildShader.modelMatrix, matrix);
+            graphics->setUniformFloat(buildShader.unitY, unitY);
+            graphics->setUniformFloat(buildShader.seaLevel, seaLevel);
+            graphics->setUniformBool(buildShader.shade, shaded);
+            graphics->setUniformFloat(buildShader.percentComplete, percentComplete);
+            graphics->setUniformFloat(buildShader.time, time);
 
             graphics->bindTexture(unitTextureAtlas.get());
             graphics->drawTriangles(mesh.vertices);

CMakeLists.txt
@@ -280,6 +280,8 @@ set(SOURCE_FILES
     src/rwe/MapTerrain.h
     src/rwe/Mesh.cpp
     src/rwe/Mesh.h
+    src/rwe/MeshDatabase.cpp
+    src/rwe/MeshDatabase.h
     src/rwe/MeshService.cpp
     src/rwe/MeshService.h
     src/rwe/Metal.h

src/rwe/GameScene.cpp
@@ -526,9 +526,7 @@ namespace rwe
         for (const auto& selectedUnitId : selectedUnits)
         {
             const auto& unit = getUnit(selectedUnitId);
-            const auto& fbi = unitDatabase.getUnitInfo(unit.unitType);
-            auto selectionMesh = unitDatabase.getSelectionMesh(fbi.objectName);
-            worldRenderService.drawSelectionRect(unit, *selectionMesh.value());
+            worldRenderService.drawSelectionRect(unit);
         }
 
         worldRenderService.drawUnitShadows(simulation.terrain, simulation.units | boost::adaptors::map_values);
@@ -1885,12 +1883,12 @@ namespace rwe
         return it;
     }
 
-    std::optional<float> GameScene::selectionIntersect(const Unit& unit, const SelectionMesh& mesh, const Ray3f& ray) const
+    std::optional<float> GameScene::selectionIntersect(const Unit& unit, const CollisionMesh& mesh, const Ray3f& ray) const
     {
         auto inverseTransform = toFloatMatrix(unit.getInverseTransform());
         auto line = ray.toLine();
         Line3f modelSpaceLine(inverseTransform * line.start, inverseTransform * line.end);
-        auto v = mesh.collisionMesh.intersectLine(modelSpaceLine);
+        auto v = mesh.intersectLine(modelSpaceLine);
         if (!v)
         {
             return std::nullopt;

src/rwe/GameScene.h
@@ -396,7 +396,7 @@ namespace rwe
          * The value returned is the distance along the ray
          * where the intersection occurred.
          */
-        std::optional<float> selectionIntersect(const Unit& unit, const SelectionMesh& mesh, const Ray3f& ray) const;
+        std::optional<float> selectionIntersect(const Unit& unit, const CollisionMesh& mesh, const Ray3f& ray) const;
 
         std::optional<SimVector> getMouseTerrainCoordinate() const;
 

src/rwe/GraphicsContext.h
@@ -8,7 +8,6 @@
 #include <rwe/MapFeature.h>
 #include <rwe/MapTerrain.h>
 #include <rwe/Mesh.h>
-#include <rwe/SelectionMesh.h>
 #include <rwe/ShaderHandle.h>
 #include <rwe/ShaderProgramHandle.h>
 #include <rwe/Sprite.h>

src/rwe/LoadingScene.cpp
@@ -4,6 +4,7 @@
 #include <rwe/Index.h>
 #include <rwe/WeaponTdf.h>
 #include <rwe/atlas_util.h>
+#include <rwe/geometry/CollisionMesh.h>
 #include <rwe/ota.h>
 #include <rwe/tdf.h>
 #include <rwe/tnt/TntArchive.h>
@@ -163,7 +164,7 @@ namespace rwe
         auto atlasInfo = createTextureAtlases(sceneContext.vfs, sceneContext.graphics, sceneContext.palette);
         MeshService meshService(sceneContext.vfs, sceneContext.graphics, std::move(atlasInfo.textureAtlasMap), std::move(atlasInfo.teamTextureAtlasMap), std::move(atlasInfo.colorAtlasMap));
 
-        auto unitDatabase = createUnitDatabase(meshService);
+        auto [unitDatabase, meshDatabase] = createUnitDatabase(meshService);
 
         MovementClassCollisionService collisionService;
 
@@ -220,7 +221,7 @@ namespace rwe
 
         auto gameNetworkService = std::make_unique<GameNetworkService>(*localPlayerId, std::stoi(gameParameters.localNetworkPort), endpointInfos, playerCommandService.get());
 
-        RenderService worldRenderService(sceneContext.graphics, sceneContext.shaders, worldCamera, atlasInfo.textureAtlas, std::move(atlasInfo.teamTextureAtlases));
+        RenderService worldRenderService(sceneContext.graphics, sceneContext.shaders, std::move(meshDatabase), worldCamera, atlasInfo.textureAtlas, std::move(atlasInfo.teamTextureAtlases));
         UiRenderService worldUiRenderService(sceneContext.graphics, sceneContext.shaders, worldUiCamera);
         UiRenderService chromeUiRenderService(sceneContext.graphics, sceneContext.shaders, chromeUiCamera);
 
@@ -556,9 +557,10 @@ namespace rwe
         return it->second;
     }
 
-    UnitDatabase LoadingScene::createUnitDatabase(MeshService& meshService)
+    std::pair<UnitDatabase, MeshDatabase> LoadingScene::createUnitDatabase(MeshService& meshService)
     {
         UnitDatabase db;
+        MeshDatabase meshDb;
 
         // read sound categories
         {
@@ -642,7 +644,7 @@ namespace rwe
                         db.addUnitModelDefinition(pair.second.model, std::move(meshInfo.modelDefinition));
                         for (const auto& m : meshInfo.pieceMeshes)
                         {
-                            db.addUnitPieceMesh(pair.second.model, m.first, m.second);
+                            meshDb.addUnitPieceMesh(pair.second.model, m.first, m.second);
                         }
                     }
 
@@ -685,10 +687,11 @@ namespace rwe
                 db.addUnitModelDefinition(fbi.objectName, std::move(meshInfo.modelDefinition));
                 for (const auto& m : meshInfo.pieceMeshes)
                 {
-                    db.addUnitPieceMesh(fbi.objectName, m.first, m.second);
+                    meshDb.addUnitPieceMesh(fbi.objectName, m.first, m.second);
                 }
 
-                db.addSelectionMesh(fbi.objectName, std::make_shared<SelectionMesh>(std::move(meshInfo.selectionMesh)));
+                db.addSelectionMesh(fbi.objectName, std::make_shared<CollisionMesh>(std::move(meshInfo.selectionMesh.collisionMesh)));
+                meshDb.addSelectionMesh(fbi.objectName, std::make_shared<GlMesh>(std::move(meshInfo.selectionMesh.visualMesh)));
             }
         }
 
@@ -713,7 +716,7 @@ namespace rwe
             }
         }
 
-        return db;
+        return std::make_pair(db, meshDb);
     }
 
     void LoadingScene::preloadSound(UnitDatabase& db, const std::optional<std::string>& soundName)

src/rwe/LoadingScene.h
@@ -141,7 +141,7 @@ namespace rwe
 
         const SideData& getSideData(const std::string& side) const;
 
-        UnitDatabase createUnitDatabase(MeshService& meshService);
+        std::pair<UnitDatabase, MeshDatabase> createUnitDatabase(MeshService& meshService);
 
         void preloadSound(UnitDatabase& db, const std::string& soundName);
 

src/rwe/MeshDatabase.cpp
@@ -1,0 +1,35 @@
+#include "MeshDatabase.h"
+
+namespace rwe
+{
+    void MeshDatabase::addSelectionMesh(const std::string& objectName, std::shared_ptr<GlMesh> mesh)
+    {
+        selectionMeshesMap.insert({objectName, mesh});
+    }
+
+    std::optional<std::shared_ptr<GlMesh>> MeshDatabase::getSelectionMesh(const std::string& objectName) const
+    {
+        auto it = selectionMeshesMap.find(objectName);
+        if (it == selectionMeshesMap.end())
+        {
+            return std::nullopt;
+        }
+        return it->second;
+    }
+
+    void MeshDatabase::addUnitPieceMesh(const std::string& unitName, const std::string& pieceName, std::shared_ptr<ShaderMesh> pieceMesh)
+    {
+        unitPieceMeshesMap.insert({{unitName, pieceName}, pieceMesh});
+    }
+
+    std::optional<std::shared_ptr<ShaderMesh>> MeshDatabase::getUnitPieceMesh(const std::string& objectName, const std::string& pieceName) const
+    {
+        auto it = unitPieceMeshesMap.find({objectName, pieceName});
+        if (it == unitPieceMeshesMap.end())
+        {
+            return std::nullopt;
+        }
+        return it->second;
+    }
+
+}

commit c6c2efae08890180e90ef67e149a76129c02778f

Fix bad transform when drawing building units
Regression introduced in fba7d27d67459e67d75b75dc08aed575270c1137

src/rwe/RenderService.cpp
@@ -109,7 +109,7 @@ namespace rwe
         if (mesh.visible)
         {
             const auto& resolvedMesh = *meshDatabase.getUnitPieceMesh(objectName, mesh.name).value();
-            drawBuildingShaderMesh(resolvedMesh, modelMatrix, seaLevel, mesh.shaded, percentComplete, unitY, time, playerColorIndex);
+            drawBuildingShaderMesh(resolvedMesh, matrix, seaLevel, mesh.shaded, percentComplete, unitY, time, playerColorIndex);
         }
 
         for (const auto& c : mesh.children)