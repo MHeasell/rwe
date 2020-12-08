c776d182d8f8486cbbaf939409decb44bc95492e
Michael Heasell <michael.heasell@gmail.com>
2020-12-08 21:41:56 +0000

    Move HPI template functions to bottom
    These need to come after the function declarations
    that they rely on.

--- src/rwe/io/hpi/hpi_util.h
+++ src/rwe/io/hpi/hpi_util.h
@@ -10,20 +10,6 @@ namespace rwe
         explicit HpiException(const char* message);
     };
 
-    template <typename T>
-    T readAndDecryptRaw(std::istream& stream, unsigned char key)
-    {
-        T val;
-        readAndDecrypt(stream, key, reinterpret_cast<char*>(&val), sizeof(T));
-        return val;
-    }
-
-    template <typename T>
-    void readAndDecryptRawArray(std::istream& stream, unsigned char key, T* buffer, std::size_t size)
-    {
-        readAndDecrypt(stream, key, reinterpret_cast<char*>(buffer), sizeof(T) * size);
-    }
-
     void decrypt(unsigned char key, unsigned char seed, char buf[], std::streamsize size);
 
     void readAndDecrypt(std::istream& stream, unsigned char key, char buf[], std::streamsize size);
@@ -41,4 +27,18 @@ namespace rwe
     std::optional<std::size_t> stringSize(const char* begin, const char* end);
 
     void extractCompressed(std::istream& stream, unsigned char decryptionKey, char* buffer, std::size_t size);
+
+    template <typename T>
+    T readAndDecryptRaw(std::istream& stream, unsigned char key)
+    {
+        T val;
+        readAndDecrypt(stream, key, reinterpret_cast<char*>(&val), sizeof(T));
+        return val;
+    }
+
+    template <typename T>
+    void readAndDecryptRawArray(std::istream& stream, unsigned char key, T* buffer, std::size_t size)
+    {
+        readAndDecrypt(stream, key, reinterpret_cast<char*>(buffer), sizeof(T) * size);
+    }
 }
