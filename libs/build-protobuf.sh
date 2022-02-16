93f2b39769fb5a8ef6ce6e08c7fdb0056fac3450
Kevin Hake <kwhake@buffalo.edu>
2022-02-15 22:45:50 -0600

    derp

--- libs/build-protobuf.sh
+++ libs/build-protobuf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-
+echo "starting build-protobuf.sh"
 install_dir="${PWD}/_protobuf-install"
 
 if [ ! -f "$install_dir/done" ]; then
@@ -11,3 +11,4 @@ if [ ! -f "$install_dir/done" ]; then
     make install
     touch "$install_dir/done"
 fi
+echo "leaving build-protobuf.sh"
