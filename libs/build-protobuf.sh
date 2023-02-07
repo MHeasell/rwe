d111483666de8f2fdcad624f2e2b69d059cf8a3c
Michael Heasell <michael.heasell@gmail.com>
2023-02-07 22:44:54 +0000

    Rebuild protobuf when commit changes

--- libs/build-protobuf.sh
+++ libs/build-protobuf.sh
@@ -3,11 +3,27 @@ set -e
 
 install_dir="${PWD}/_protobuf-install"
 
-if [ ! -f "$install_dir/done" ]; then
-    cd protobuf
+cd protobuf
+source_protobuf_version="$(git rev-parse HEAD)"
+
+built_protobuf_version=""
+
+if [ -f "$install_dir/done" ]; then
+    built_protobuf_version="$(cat "$install_dir/done")"
+fi
+
+if [ "$source_protobuf_version" != "$built_protobuf_version" ]; then
+    echo "built protobuf is a different version than source"
+    echo "source protobuf: $source_protobuf_version"
+    echo "built protobuf:  $built_protobuf_version"
+    echo "cleaning and rebuilding protobuf"
+    rm -rf "$install_dir"
     ./autogen.sh
     ./configure "--prefix=$install_dir"
     make -j`nproc`
     make install
-    touch "$install_dir/done"
+    echo "$protobuf_version" > "$install_dir/done"
+else
+    echo "built protobuf matches source protobuf, skipping protobuf build"
+    echo "built protobuf: $built_protobuf_version"
 fi
