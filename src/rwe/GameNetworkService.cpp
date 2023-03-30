d8fa42c212b7f582436a64c1196f250cdc246cc5
Michael Heasell <michael.heasell@gmail.com>
2023-03-30 15:04:23 +0100

    Move range_util.h into util directory

--- src/rwe/GameNetworkService.cpp
+++ src/rwe/GameNetworkService.cpp
@@ -1,9 +1,9 @@
 #include "GameNetworkService.h"
+#include "rwe/util/range_util.h"
 #include <boost/range/adaptors.hpp>
 #include <rwe/GameHash.h>
 #include <rwe/network_util.h>
 #include <rwe/proto/serialization.h>
-#include <rwe/range_util.h>
 #include <rwe/sim/SimTicksPerSecond.h>
 #include <rwe/util/Index.h>
 #include <rwe/util/OpaqueId_io.h>
